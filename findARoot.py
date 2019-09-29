bf8cdcac746bee02402a0e822cb55605ae188aea
Michael Welch <michael.g.welch@jci.com>
2019-09-29 15:54:10 -0500

    Remove local var answer

--- findARoot.py
+++ findARoot.py
@@ -2,26 +2,23 @@
 def findARoot(value):
     if (value == 0):
         return [0,2]
-    answer = []
     if (value > 0):
         root = 2
         increment = 1
     else:
         root = -2
         increment = -1
-    guess = 0
-    while not answer:
+    while True:
         power = 2
         if abs(root)**power > abs(value):
-            break
+            return []
         while power < 6:
             guess = root**power
             if guess == value:
-                answer = [root, power]
-                break
+                return [root, power]
             elif abs(guess) > abs(value):
                 break
             power = power + 1
         root = root + increment
-    return answer
+    return []
 
